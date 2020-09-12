dc184f734ad25081a1a4d5b7edcc5ff7613b5471
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-09-12 18:11:10 +0530

    Api set up for notifications

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
-const os = require("os");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+//Config file
+dotenv.config({ path: "./config/config.env" });
+
+// Body Parser
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.json({ limit: "50mb" }));
+
+// Cors
+app.use(cors());
+
+// Morgan for Logging
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.static("dist"));
-app.get("/api/getUsername", (req, res) =>
-  res.send({ username: os.userInfo().username })
-);
+
+// Routes
+app.use("/api", require("./routes/index.js"));
 
 app.listen(process.env.PORT || 8080, () =>
   console.log(`Listening on port ${process.env.PORT || 8080}!`)
---
+++ src/server/routes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios").default;
+
+/**
+ * @description Fetch notifications
+ * @route GET https://api.rootnet.in/covid19-in/notifications
+ */
+
+router.get("/notifications", async (req, res) => {
+  let fetchedData;
+  await axios
+    .get("https://api.rootnet.in/covid19-in/notifications")
+    .then(function (response) {
+      // handle success
+      fetchedData = response.data;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    });
+
+  res.status(200).json(fetchedData.data);
+});
+
+module.exports = router;
