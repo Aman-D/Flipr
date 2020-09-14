aea6c29018a39a6209c420ac5c3ad268d965b360
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-09-14 20:33:24 +0530

    Add / path

--- src/server/index.js
+++ src/server/index.js
@@ -20,6 +20,10 @@ if (process.env.NODE_ENV == "development") {
 
 app.use(express.static("dist"));
 
+app.get("/", function (req, res) {
+  res.sendFile("index.html");
+});
+
 // Routes
 app.use("/api", indexRoute);
 
